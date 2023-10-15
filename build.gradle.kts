8b98fdd165c082239d2ba2faa129df7c7ec2f4cf
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2023-10-15 02:11:54 +0200

    Dependencies and plugins versions bump.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.1.1" apply false
+    id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
 
@@ -10,7 +10,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle-api:8.0.2")
+        classpath("com.android.tools.build:gradle-api:8.1.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
     }
