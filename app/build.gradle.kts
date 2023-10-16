d6c93920173e8436415e533d821cf7d29564e796
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2023-10-16 21:17:27 +0200

    The minimum SDK version has been lowered to 24.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.andrii_a.lumos"
-        minSdk = 27
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
