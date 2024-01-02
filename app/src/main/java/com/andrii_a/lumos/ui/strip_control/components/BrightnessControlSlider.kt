040ece9bcc8334828761112d7269da675b753f87
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2024-01-02 12:50:13 +0100

    Changed BrightnessControlSlider look and feel.

--- app/src/main/java/com/andrii_a/lumos/ui/strip_control/components/BrightnessControlSlider.kt
+++ app/src/main/java/com/andrii_a/lumos/ui/strip_control/components/BrightnessControlSlider.kt
@@ -1,24 +1,29 @@
 package com.andrii_a.lumos.ui.strip_control.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Brightness5
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.andrii_a.lumos.R
+import com.andrii_a.lumos.ui.theme.LumosTheme
 
 @Composable
 fun BrightnessControlSlider(
@@ -27,35 +32,69 @@ fun BrightnessControlSlider(
 ) {
     var sliderPosition by rememberSaveable { mutableFloatStateOf(0f) }
 
-    Row(
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
-    ) {
-        Text(
-            text = stringResource(id = R.string.brightness),
-            style = MaterialTheme.typography.titleMedium,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis
+    ConstraintLayout(modifier = modifier) {
+        val (brightnessIcon, slider, brightnessValueText) = createRefs()
+
+        Icon(
+            imageVector = Icons.Outlined.Brightness5,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.secondaryContainer,
+            modifier = Modifier
+                .constrainAs(brightnessIcon) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                }
+                .background(
+                    color = MaterialTheme.colorScheme.secondary,
+                    shape = CircleShape
+                )
+                .padding(8.dp)
+        )
+
+        Slider(
+            value = sliderPosition,
+            onValueChange = { sliderPosition = it },
+            valueRange = 0f..100f,
+            onValueChangeFinished = {
+                onBrightnessSet(sliderPosition)
+            },
+            modifier = Modifier.constrainAs(slider) {
+                top.linkTo(brightnessIcon.top)
+                bottom.linkTo(brightnessIcon.bottom)
+                start.linkTo(brightnessIcon.end, 16.dp)
+                end.linkTo(brightnessValueText.start, 16.dp)
+
+                width = Dimension.fillToConstraints
+            }
         )
 
         Text(
             text = stringResource(
                 id = R.string.brightness_level_formatted,
                 sliderPosition.toInt()
-            )
+            ),
+            modifier = Modifier.constrainAs(brightnessValueText) {
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+                end.linkTo(parent.end)
+            }
         )
+
     }
+}
 
-    Spacer(modifier = Modifier.height(8.dp))
-
-    Slider(
-        value = sliderPosition,
-        onValueChange = { sliderPosition = it },
-        valueRange = 0f..100f,
-        onValueChangeFinished = {
-            onBrightnessSet(sliderPosition)
-        },
-        modifier = Modifier.padding(horizontal = 16.dp)
-    )
+@Preview
+@Composable
+fun BrightnessControlSliderPreview() {
+    LumosTheme {
+        Surface {
+            BrightnessControlSlider(
+                onBrightnessSet = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+        }
+    }
 }
