f68f91fafc956c7b83c077f002245373cd8a94cf
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2024-06-15 23:48:28 +0200

    Dependencies update.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,8 +64,8 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.2")
     implementation("androidx.activity:activity-compose:1.9.0")
 
     // Jetpack Compose
